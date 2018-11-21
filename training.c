2ae3431d356bdd03c7600719f31b34017243bb42
nutthapooms <nutthapoom.s@hotmail.com>
2018-11-22 00:30:52 +0700

    add search in file & parse string+return

--- training.c
+++ training.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <string.h>
+#define MAXCHAR 5
 
-#define MAXCHAR 4
+char * parseStringg(char* stringg){
+    char * asd = "asdasdasd";
+    return asd;
+}
+
 int main() {
     FILE *fp;
     char str[MAXCHAR];
@@ -11,12 +17,29 @@ int main() {
         printf("Could not open file %s",filename);
         return 1;
     }
-    fprintf(fp,"1asdasd23");
+    //fprintf(fp,"1asdasd23");
     fclose(fp);
     fp = fopen(filename, "r+");
-    fgets(str, MAXCHAR, fp);
-    printf("%s", str);
+     fgets(str, MAXCHAR, fp);
+    char c = "";
+    while(c!= EOF){
+        c = fgetc(fp);
+        printf("%d \n",c);
+    }
+
+    printf("%s \n", parseStringg(str));
     fclose(fp);
+
+    fp=fopen("text.txt","r");
+    char tmp[256]={0x0};
+    while(fp && fgets(tmp, sizeof(tmp), fp))
+    {
+        if (strstr(tmp, "haha"))
+            printf("%s", tmp);
+        else if (strstr(tmp, "hehe"))
+            printf("%s", tmp);
+    }
+    if(fp) fclose(fp);
     return 0;
 }
 
