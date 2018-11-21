8dfeca6a4f772bcf7159c0ae15eddc989aee0645
nutthapooms <nutthapoom.s@hotmail.com>
2018-11-22 00:13:53 +0700

    try fopen(a+)

--- training.c
+++ training.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
-#define MAXCHAR 5
+#define MAXCHAR 4
 int main() {
     FILE *fp;
     char str[MAXCHAR];
     char* filename = "text.txt";
 
-    fp = fopen(filename, "r+");
+    fp = fopen(filename, "a+");
     if (fp == NULL){
         printf("Could not open file %s",filename);
         return 1;
     }
-    fprintf(fp,"123111111111123123");
-    while (fgets(str, MAXCHAR, fp) != NULL)
+    fprintf(fp,"1asdasd23");
+    fclose(fp);
+    fp = fopen(filename, "r+");
+    fgets(str, MAXCHAR, fp);
     printf("%s", str);
     fclose(fp);
     return 0;
 }
+
